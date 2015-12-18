ddf84242a78e12336e2d64e300a48bf6a49fdbbc
DanielBadura <d.m.badura@googlemail.com>
2015-12-18 15:25:06 +0000

    added tests, added fixtures

--- tests/Repository/IssueRepositoryTest.php
+++ tests/Repository/IssueRepositoryTest.php
@@ -3,13 +3,50 @@
 namespace DanielBadura\Redmine\Api\Tests\Repository;
 
 use DanielBadura\Redmine\Api\Tests\ClientTest;
+use ProxyManager\Proxy\LazyLoadingInterface;
 
 class IssueRepositoryTest extends ClientTest
 {
     public function testFindIssue()
     {
-        $issue = $this->client->getIssueRepository()->find(10);
+        $issue = $this->client->getIssueRepository()->find(1);
 
-        dump($issue);
+        $this->assertEquals(1, $issue->getId());
+    }
+
+    public function testProjectLazyLoad()
+    {
+        $issue   = $this->client->getIssueRepository()->find(1);
+        $project = $issue->getProject();
+        $id      = $project->getId();
+
+        $this->assertEquals(1, $id);
+        $this->assertTrue($project instanceof LazyLoadingInterface);
+        $this->assertTrue($project->getProxyInitializer() instanceof \Closure);
+
+        $name = $project->getName();
+        $this->assertEquals("Test", $name);
+        $this->assertTrue($project->getProxyInitializer() instanceof \Closure);
+
+        $id = $project->getIdentifier();
+        $this->assertEquals('test', $id);;
+        $this->assertTrue($project->getProxyInitializer() === null);
+    }
+
+    public function testUserLazyLoad()
+    {
+        $issue = $this->client->getIssueRepository()->find(1);
+        $user  = $issue->getAuthor();
+        $id    = $user->getId();
+
+        $this->assertEquals(1, $id);
+        $this->assertTrue($user instanceof LazyLoadingInterface);
+        $this->assertTrue($user->getProxyInitializer() instanceof \Closure);
+
+        dump($user);
+
+        $name = $user->getLogin();
+        $this->assertEquals("admin", $name);
+        $this->assertTrue($user->getProxyInitializer() === null);
     }
 }
